eaa217ca88e44cafdf9be77cad2294efa3bb7155
WinriseF <3098274040@qq.com>
2026-03-22 16:35:29 +0800

    fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,11 @@
 fn main() {
+    let frontend_dist = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("../dist");
+    if let Err(err) = std::fs::create_dir_all(&frontend_dist) {
+        panic!(
+            "failed to create frontendDist placeholder at {}: {err}",
+            frontend_dist.display()
+        );
+    }
+
     tauri_build::build()
 }
